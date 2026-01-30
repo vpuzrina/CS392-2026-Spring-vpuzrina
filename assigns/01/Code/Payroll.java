07f94c777334df494f9bba7753269062cba09d2d
Varvara <varvara@crc-dot1x-nat-10-239-95-131.bu.edu>
2026-01-30 18:10:07 -0500

    Assign01 completed

--- assigns/01/Code/Payroll.java
+++ assigns/01/Code/Payroll.java
@@ -3,31 +3,82 @@ public class Payroll {
     public static final int INITIAL_MAXIMUM_SIZE = 1024;
 
     public Payroll() {
-	/* your code */
+	people= new Employee[INITIAL_MAXIMUM_SIZE];   
+    maximum_size = INITIAL_MAXIMUM_SIZE;  
+    current_size=0;
+    } 
+    public int size() { 
+        return current_size; 
+    } 
+
+    public void print() { 
+        for (int i = 0 ; i < current_size; i++) { 
+            Employee e = people[i]; 
+            System.out.println(e.name + " " + e.ID + " " + e.salary); 
+        } 
+    
+
     }
     
     public void add_employee(Employee newbie) {
-	/* your code */
-    }
+	    if (current_size == maximum_size) { 
+            double_size(); 
+        } 
+        people[current_size] = newbie; 
+        current_size++; 
+    } 
+
 
     public void remove_employee(int i) throws EmployeeIndexException {
-	/* your code */
+	    if (i<0 || i >= current_size) { 
+            throw new EmployeeIndexException(); 
+        } 
+        people[i] = people[current_size - 1];  
+        people[current_size - 1] = null; 
+        current_size--; 
+
+
     }
     
     public int find_employee(String name) throws EmployeeNotFoundException {
-	/* your code */
+        for (int i =0 ; i < current_size; i++) { 
+            if (people[i].name.equals(name)) { 
+                return i; 
+            } 
+        } 
+        throw new EmployeeNotFoundException(); 
+
     }
 
     public void add_payroll(Payroll source) {
-	/* your code */
+        for (int i = 0; i < source.current_size; i++) { 
+            add_employee(source.people[i]);
+        } 
+            
     }
 
     public void copy_payroll(Payroll source) {
-	/* your code */
-    }
+        people = new Employee[INITIAL_MAXIMUM_SIZE]; 
+        maximum_size = INITIAL_MAXIMUM_SIZE; 
+        current_size= source.current_size; 
+        for (int i = 0; i <current_size; i++) { 
+            people[i] = source.people[i]; 
+        }
+
+    } 
+
+    private void double_size () {
+        Employee[] big = new Employee[maximum_size*2]; 
+        for(int i = 0; i <current_size; i++) { 
+            big[i] = people[i]; 
+        } 
+        people = big; 
+        maximum_size *= 2; 
+
+    } 
 
     private Employee people[];
     private int maximum_size;
     private int current_size;
-    ...
+   
 }
--- assigns/01/Code/TestPayroll.java
+++ assigns/01/Code/TestPayroll.java
@@ -1,4 +1,29 @@
 
 public class TestPayroll {
-    /* your code */
+    public static void main(String[] args) { 
+
+        Payroll p = new Payroll(); 
+        Employee e1 = new Employee(); 
+        e1.name = "Varvara"; 
+        e1.ID = 1; 
+        e1.salary = 1000; 
+
+        Employee e2 = new Employee(); 
+        e2.name = "Andrey"; 
+        e2.ID = 2; 
+        e2.salary = 2500; 
+        p.add_employee(e1);
+        p.add_employee(e2);
+
+        p.print(); 
+
+        try { 
+            int i = p.find_employee("Varvara"); 
+            p.remove_employee(i);
+        } catch (Exception e) { 
+            System.out.println("Exception caught");
+        } 
+
+        System.out.println("Finished"); 
+    } 
 }
