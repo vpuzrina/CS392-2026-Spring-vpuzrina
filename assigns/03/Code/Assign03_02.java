a5f958be7d7aa27bacb0322184288000b10c0996
Varvara <varvara@crc-dot1x-nat-10-239-95-131.bu.edu>
2026-02-10 21:51:47 -0500

    Assignment 3-2: bracket balance using FnList

--- assigns/03/Code/Assign03_02.java
+++ assigns/03/Code/Assign03_02.java
@@ -1,18 +1,31 @@
 /*
 HX-2026-02-05: 10 points
 */
+import Library00.FnList.FnList;
 public class Assign03_02 {
     static boolean balencedq(String text) {
-	//
-	// There are only '(', ')', '[', ']', '{', and '}'
-	// appearing in [text]. This method should return
-	// true if and only if the parentheses/brackets/braces
-	// in [text] are balenced.
-	// Your solution must make proper use of FnList (as a stack)!
-	//
+		FnList<Character> stack= FnList.nil;
+		for (int i=0; i < text.length();i++){ 
+			char c =text.charAt(i);
+			if (c == '(' ||c=='[' || c == '{') { 
+				stack = stack.cons(c); 
+			}
+			else { 
+				if (FnList.isNil(stack)) return false; 
+				char k = stack.getHead();
+				stack =stack.getTail(); 
+				if ((c==')' && k!='(') || (c == ']' && k!='[')||(c == '}' && k!='{')) { return false; 
+				}
+			}	
+
+		}
+		return FnList.isNil(stack);
     }
 
     public static void main(String[] argv) {
-	// Please write some testing code for your 'balencedq"
+	System.out.println(balencedq("({()[({})]})")); 
+	System.out.println(balencedq("({()[({})])}")); 
+	System.out.println(balencedq("(){}[]")); 
+	System.out.println(balencedq("({]{")); 
     }
 }
