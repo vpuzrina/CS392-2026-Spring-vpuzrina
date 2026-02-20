6d4faa1d349a58b0c5fad8872e1dab7fffab1f13
Varvara <varvara@Varvaras-MacBook-Air.local>
2026-02-19 22:51:44 -0500

    Complete Assignment 4

--- assigns/04/Code/Assign04_01.java
+++ assigns/04/Code/Assign04_01.java
@@ -4,18 +4,44 @@ HX-2026-02-13: 10 points
 import MyLibrary.FnList.*;
 import MyLibrary.FnStrn.*;
 
-public class Assign04_01 {
-    static boolean balencedq(String text) {
-	//
-	// There are only '(', ')', '[', ']', '{', and '}'
-	// appearing in [text]. This method should return
-	// true if and only if the parentheses/brackets/braces
-	// in [text] are balenced.
-	// Your solution must make proper use of FnList (as a stack)!
-	//
+public class Assign04_01 { 
+	static class State { 
+		FnList<Character> st=FnList.<Character>nil(); 
+		boolean ok =true; 
+	}
+    static boolean balencedq(String text) { 
+		final State s = new State(); 
+
+		FnStrn.forall(text,(c) -> { 
+			if (!s.ok) return false; 
+
+			if (c == '(' || c == '['  || c== '{') { 
+				s.st = FnList.cons (c, s.st); 
+				return true; 
+			} 
+
+			if (FnList.isNil(s.st)) { 
+				s.ok =false; 
+				return false; 
+			} 
+			char top = s.st.head(); 
+			s.st =s.st.tail(); 
+			if ((c== ')' && top != '(') || (c== ']' && top != '[') || (c== '}' && top != '{')) { 
+				s.ok=false; 
+				return false; 
+			} 
+			return true; 
+		}); 
+		return s.ok&& FnList.isNil(s.st); 
+
     }
 
     public static void main(String[] argv) {
-	// Please write some testing code for your 'balencedq"
+	System.err.println(balencedq("{{]](]}})}}[}"));
+	System.err.println(balencedq("{{}](())}}[[]"));
+	System.err.println(balencedq("[][()({}")); 
+	System.err.println(balencedq("{}}{))){}[]")); 
+	System.err.println(balencedq("{{}{}(()"));
+
     }
 } // end of [public class Assign04_01{...}]
--- assigns/04/Code/Assign04_02.java
+++ assigns/04/Code/Assign04_02.java
@@ -7,15 +7,17 @@ import MyLibrary.FnStrn.*;
 public class Assign04_02 {
     static FnStrn
 	FnList$FnStrn_concate(FnList<FnStrn> xs) {
-	// Given a list of strings, this method return the
-	// concatenation of these string. For instance, given
-	// ("a", "bc", "def"), the returned string is "abcdef"
-	// You implementation is NOT allowed to use loops or
-	// reccursion. Try to use the 'foritm' method in FnList
-	// and FnStrn to accomplish this task.
+		return FnList.foritm( 
+			xs, 
+			new FnStrn(""), 
+			(acc,s ) -> FnStrn.concat(acc,s) 
+		); 
     }
 
-    public static void main(String[] argv) {
-	// Please write some testing code for your 'FnList$FnStrn_concate"
+    public static void main(String[] argv) { 
+		FnList<FnStrn> xs = 
+			FnList.cons(new FnStrn("a"), FnList.cons(new FnStrn("ab"), FnList.cons(new FnStrn("dif"), FnList.nil()))); 
+		FnStrn result = FnList$FnStrn_concate(xs);
+		System.err.println(result);
     }
 } // end of [public class Assign04_02{...}]
