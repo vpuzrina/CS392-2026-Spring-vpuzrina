f5e2a74dada50719e1ff423639c9b7a3ca6cda84
Varvara <varvara@crc-dot1x-nat-10-239-95-131.bu.edu>
2026-02-20 19:47:45 -0500

    Updated solution for Assign03 and Assign04

--- assigns/03/Code/Assign03_02.java
+++ assigns/03/Code/Assign03_02.java
@@ -1,25 +1,25 @@
 /*
 HX-2026-02-05: 10 points
 */
-import Library00.FnList.FnList;
+import Library00.FnList.*;
 public class Assign03_02 {
     static boolean balencedq(String text) {
-		FnList<Character> stack= FnList.nil;
+		FnList<Character> stack = FnListSUtil.nil();
 		for (int i=0; i < text.length();i++){ 
 			char c =text.charAt(i);
 			if (c == '(' ||c=='[' || c == '{') { 
-				stack = stack.cons(c); 
+				stack = FnListSUtil.cons(c, stack); 
 			}
-			else { 
-				if (FnList.isNil(stack)) return false; 
-				char k = stack.getHead();
-				stack =stack.getTail(); 
+			else if (c == ')' || c == ']' || c == '}') { 
+				if (stack.nilq()) return false; 
+				char k = stack.hd();
+				stack = stack.tl(); 
 				if ((c==')' && k!='(') || (c == ']' && k!='[')||(c == '}' && k!='{')) { return false; 
 				}
-			}	
+			}
 
 		}
-		return FnList.isNil(stack);
+		return stack.nilq();
     }
 
     public static void main(String[] argv) {
