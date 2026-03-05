f12ebb555f49dd1f8f3baddf0f592d4175b9790e
Varvara <varvara@crc-dot1x-nat-10-239-95-131.bu.edu>
2026-03-05 16:39:19 -0500

    Quiz01_01 solution

--- quizzes/01/Code/Quiz01_01.java
+++ quizzes/01/Code/Quiz01_01.java
@@ -8,17 +8,41 @@ public class Quiz01_01 {
     public static
 	<T extends Comparable<T>>
 	int FnA1szBinarySearch(FnA1sz<T> A, T key) {
-	// HX-2026-03-03:
-	// Please implement binary search on a sorted functional array (FnA1sz)
-	// that returns the largest index i such that key >= A[i] if such i exists,
-	// or the method returns -1. The comparison function should be the compareTo
-	// method implemented by the class T.
-	return 0;
+		int n = A.length();
+		int lo= 0, hi = n - 1;
+		int ans = -1;
+		while (lo<= hi) {
+			int mid = lo+(hi-lo) / 2;
+			T v = A.getAt(mid); 
+			if (key.compareTo(v) >= 0) {
+				ans = mid;
+				lo= mid + 1; 
+
+			} else { 
+				hi = mid - 1;
+			}
+		} 
+
+	return ans;
     }
     public static void main (String[] args) {
-	// HX-2026-03-04:
-	// Please write minimal testing code for FnA1szBinarySearch
-	// Should test for cases T = Integer and T = String
-	return /*void*/;
+		Integer [] a1 = new Integer [] {1, 3, 3, 7};
+		FnA1sz<Integer> A1=new FnA1sz<>(a1);
+		System.out.println(FnA1szBinarySearch(A1, 0));
+		System.out.println(FnA1szBinarySearch(A1, 1));
+		System.out.println(FnA1szBinarySearch(A1, 3));
+		System.out.println(FnA1szBinarySearch(A1, 7));
+		System.out.println(FnA1szBinarySearch(A1, 99));
+		System.out.println(FnA1szBinarySearch(A1, 4)); 
+
+		String [] a2= new String [] {"a", "b", "b","d"};
+		FnA1sz<String>A2= new FnA1sz<> (a2);
+		System.out.println(FnA1szBinarySearch(A2, "0")); 
+		System.out.println(FnA1szBinarySearch(A2, "v"));
+		System.out.println(FnA1szBinarySearch(A2, "a"));
+		System.out.println(FnA1szBinarySearch(A2, "r"));
+		System.out.println(FnA1szBinarySearch(A2, "p"));
+		System.out.println(FnA1szBinarySearch(A2, "u"));
+	return;
     }
 }
