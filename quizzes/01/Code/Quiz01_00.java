5343c45ca3411571cbbb5c3b14e2c194500a5fde
Varvara <varvara@crc-dot1x-nat-10-239-95-131.bu.edu>
2026-03-05 20:39:28 -0500

    Quiz01_00 solution

--- quizzes/01/Code/Quiz01_00.java
+++ quizzes/01/Code/Quiz01_00.java
@@ -1,20 +1,17 @@
 //
 // HX: 10 points
 //
-/*
+
 import MyLibrary.FnList.*;
-// Add for imports as you see fit
-*/
+
 public class Quiz01_00 {
     /*
-     Please give a description of your MyLibrary
-     What classes have you implemented? For each class
-     you have implemented in MyLibrary, please create an
-     object of that class as follows:
+    MyLibrary contains implemented functional data structures. 
+    To give an example, FnList is a functional list. And it supports 
+    operations such as: head, tail and iteration methods
      */
     public static void main (String[] args) {
-	// For instance, 
-	// FnList<Integer> FnList_Integer_obj = new FnList<Integer>();
+        FnList<Integer> list =new FnList<Integer>();
 	return /*void*/;
     }
 }
