31ab49144b849eac176f9b8a705bc19aa04a5dab
Varvara <varvara@crc-dot1x-nat-10-239-145-31.bu.edu>
2026-04-28 13:34:16 -0400

    Implement Quiz02_02 insertion sort

--- quizzes/02/MySolution/Quiz02_02.java
+++ quizzes/02/MySolution/Quiz02_02.java
@@ -18,10 +18,57 @@ public class Quiz02_02 {
 	// Please implement a sorting algorithm
 	// WITHOUT recursion that can effectively
 	// sort A.
+	for (int i = 1; i < A.length; i += 1) {
+	    T key = A[i];
+	    int j = i - 1;
+	    while (j >= 0 && A[j].compareTo(key) > 0) {
+		A[j + 1] = A[j];
+		j -= 1;
+	    }
+	    A[j + 1] = key;
+	}
     }
+
+    private static <T extends Comparable<T>>
+    boolean isSorted(T[] A) {
+	for (int i = 1; i < A.length; i += 1) {
+	    if (A[i - 1].compareTo(A[i]) > 0) return false;
+	}
+	return true;
+    }
+
+    private static <T>
+    void printArray(T[] A) {
+	for (int i = 0; i < A.length; i += 1) {
+	    if (i > 0) System.out.print(" ");
+	    System.out.print(A[i]);
+	}
+	System.out.println();
+    }
+
     public static void main (String[] args) {
 	// HX-2025-11-19:
 	// Please write minimal testing code for sort1000WithNoRecursion
+	Integer[] sorted = new Integer[] {1, 2, 3, 4, 5, 6};
+	sort1000WithNoRecursion(sorted);
+	System.out.println("sorted: " + isSorted(sorted));
+	printArray(sorted);
+
+	Integer[] reverse = new Integer[] {9, 8, 7, 6, 5, 4, 3};
+	sort1000WithNoRecursion(reverse);
+	System.out.println("reverse: " + isSorted(reverse));
+	printArray(reverse);
+
+	Integer[] duplicates = new Integer[] {3, 1, 3, 2, 3, 1, 2, 2};
+	sort1000WithNoRecursion(duplicates);
+	System.out.println("duplicates: " + isSorted(duplicates));
+	printArray(duplicates);
+
+	Integer[] nearlySorted = new Integer[] {1, 2, 3, 4, 6, 5, 7, 8, 9, 10};
+	sort1000WithNoRecursion(nearlySorted);
+	System.out.println("nearlySorted: " + isSorted(nearlySorted));
+	printArray(nearlySorted);
+
 	return /*void*/;
     }
 } // end of [public class Quiz02_02{...}]
